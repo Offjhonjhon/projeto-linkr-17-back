5fc9a5b53e36c3ca7a255cb957159f0ad8651431
Offjhonjhon <jjvictorac@gmail.com>
2022-06-14 14:20:58 -0300

    doc: addition of .sql file

---
+++ config/tables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  "createdAt" TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE sessions (
+   id SERIAL PRIMARY KEY,
+   token TEXT NOT NULL UNIQUE,
+   "userId" INTEGER NOT NULL REFERENCES users(id),
+   "createdAt" TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  tag TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE publications (
+  id SERIAL PRIMARY KEY,
+  "userId" INTEGER NOT NULL REFERENCES users(id),
+  text TEXT,
+  link TEXT NOT NULL,
+  "createdAt" TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE publicationsTags (
+    id SERIAL PRIMARY KEY,
+    "publicationId" INTEGER NOT NULL REFERENCES publications(id),
+    "tagId" INTEGER NOT NULL REFERENCES tags(id)
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+     "userId" INTEGER NOT NULL REFERENCES users(id),
+    "publicationId" INTEGER NOT NULL REFERENCES publications(id)
+)
