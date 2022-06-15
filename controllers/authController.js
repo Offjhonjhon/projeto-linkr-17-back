10a3b44eb98134eb1815ffd89bc7e8c511c2cc2f
cassandra-mn <arthenciac@gmail.com>
2022-06-15 15:36:35 -0300

    test: user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,22 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import connection from "../config/db.js";
+
 export async function signUp(req, res) {
+    const {email, password, username, picture} = req.body;
+    const hash = await bcrypt.hash(password, 10);
     
-    res.sendStatus(501);
+    try {
+        const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (result.rows[0]) return res.status(409).send({error: "Email já cadastrado"});
+
+        await db.query(`INSERT INTO users(email, password, username, picture) VALUES ($1, $2, $3, $4)`, [email, hash, username, picture]);
+
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
 
 export async function signIn(req, res) {
