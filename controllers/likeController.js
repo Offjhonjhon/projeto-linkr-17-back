eb8f9a12c37260d51c90a0364748b8e534c1e7c8
Kethlyn <kethlynsaraa@gmail.com>
2022-06-15 13:13:31 -0300

    feat: check if the user has already liked the post

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,5 +1,23 @@
 import connection from "../config/db.js";
 
+export async function getUserLikes(req, res) {
+    const { publicationId } = req.body;
+    const userId = 1; //userId vai ser passado pelo res.locals no middleware de validação o token
+
+    try {        
+        const userLikes = await connection.query('SELECT * FROM likes WHERE "userId" = $1 AND "publicationId" = $2', [userId, publicationId]);
+        console.log(userLikes.rows[0])
+        if (userLikes.rows[0]) {
+            return res.send({isLiked: true});
+        } else {
+            return res.send({isLiked: false});
+        }
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500);
+    }
+}
+
 export async function postLike(req, res) {
     const { publicationId } = req.body;
     const userId = 1; //userId vai ser passado pelo res.locals no middleware de validação o token
@@ -9,10 +27,10 @@ export async function postLike(req, res) {
         
         if (rows.length === 0) {
             await connection.query('INSERT INTO likes ("userId", "publicationId") VALUES ($1, $2)', [userId, publicationId]);
-            return res.send({likeSuccess: true})
+            return res.send("likeSuccess")
         } else {
             await connection.query('DELETE FROM likes WHERE "userId" = $1 and "publicationId" = $2', [userId, publicationId]);
-            return res.send({deslikeSuccess: true})
+            return res.send("deslikeSuccess")
         }
 
     } catch (e) {
--- routes/likeRouter.js
+++ routes/likeRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { postLike } from "../controllers/likeController.js";
+import { getUserLikes, postLike } from "../controllers/likeController.js";
 import { validateLikeData } from "../middlewares/likeDataValidationMiddleware.js";
 
 const likeRouter = Router();
 
 likeRouter.post("/likes", validateLikeData ,postLike);
+likeRouter.post("/userLikes", validateLikeData, getUserLikes)
 
 export default likeRouter;
