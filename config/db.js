09bfd53b3107f67486436518b672f3b4d5c4aa6c
cassandra-mn <arthenciac@gmail.com>
2022-06-15 16:11:08 -0300

    test: config deploy

--- config/db.js
+++ config/db.js
@@ -5,12 +5,16 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const databaseConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL
+};
+
+if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
     rejectUnauthorized: false
   }
 }
+
 
 const connection = new Pool(databaseConfig);
 
