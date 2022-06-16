b2c9f903d3dd4f61912450be9ceb412acde70a15
Kethlyn <kethlynsaraa@gmail.com>
2022-06-16 16:23:47 -0300

    recycle: change query to get the userId and all the people who liked the post

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -3,15 +3,24 @@ import connection from "../config/db.js";
 export async function getUserLikes(req, res) {
     const { publicationId } = req.body;
     const userId = 1; //userId vai ser passado pelo res.locals no middleware de validação o token
+    let findUser = false;
 
-    try {        
-        const userLikes = await connection.query('SELECT * FROM likes WHERE "userId" = $1 AND "publicationId" = $2', [userId, publicationId]);
-        console.log(userLikes.rows[0])
-        if (userLikes.rows[0]) {
-            return res.send({isLiked: true});
-        } else {
-            return res.send({isLiked: false});
-        }
+    try { 
+        const { rows } = await connection.query(`
+        SELECT users.name, likes."userId" 
+        FROM likes 
+        JOIN users ON likes."userId" = users.id
+        WHERE "publicationId" = $1`, [publicationId]);
+
+        const allLikes = rows.map((row) => {
+            if (row.userId === userId) findUser = true;
+            return row.name;
+        });
+
+        console.log('arr', allLikes)
+        console.log('1', findUser)
+
+        res.send({isLiked: findUser, allLikes});
     } catch (e) {
         console.log(e)
         res.sendStatus(500);
@@ -27,10 +36,10 @@ export async function postLike(req, res) {
         
         if (rows.length === 0) {
             await connection.query('INSERT INTO likes ("userId", "publicationId") VALUES ($1, $2)', [userId, publicationId]);
-            return res.send("likeSuccess")
+            return res.send("likeSuccess");
         } else {
             await connection.query('DELETE FROM likes WHERE "userId" = $1 and "publicationId" = $2', [userId, publicationId]);
-            return res.send("deslikeSuccess")
+            return res.send("deslikeSuccess");
         }
 
     } catch (e) {
