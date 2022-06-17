1da49a70ddfad97dad2963d454acb802219c457e
José Rodolpho <j.rodolpho29@hotmail.com>
2022-06-17 17:41:19 -0300

    fix: change index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import likeRouter from "./likeRouter.js";
 import authRouter from "./authRouter.js";
+import usersProfileRouter from "./usersProfileRouter.js";
 const router = Router();
 
 router.use(likeRouter);
 router.use(authRouter);
+router.use(usersProfileRouter);
 
 export default router;
