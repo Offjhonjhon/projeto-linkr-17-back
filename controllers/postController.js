0a4aad3f2d47d855cde641be4266c0392f0f2335
Kethlyn <kethlynsaraa@gmail.com>
2022-06-18 21:23:16 -0300

    feat: query to edit post

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,16 @@ import connection from "../config/db.js";
 
 export async function editPost(req, res) {
     const { publicationId, description } = req.body;
-    console.log(publicationId, description)
-    res.sendStatus(201);
+    const userId = 2;
+
+    try {
+        await connection.query(`UPDATE publications 
+                                SET text= $1
+                                WHERE id = $2 AND "userId" = $3`,
+                                [description, publicationId, userId]);
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 }
