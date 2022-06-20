a55c1e4981ab7454d3f3d2779405df0479ee7c35
Kethlyn Sara <kethlynsaraa@gmail.com>
2022-06-19 23:54:36 -0300

    refactor: change token middleware

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,10 +1,10 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { deletePost, editPost } from '../controllers/postController.js';
-import { validateToken } from '../middlewares/tokenMiddleware.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const postRouter = Router();
 
-postRouter.post("/post-details/edit",  validateToken, editPost);
-postRouter.delete("/post-details/delete/:postId", validateToken, deletePost);
+postRouter.post("/post-details/edit",  verifyToken, editPost);
+postRouter.delete("/post-details/delete/:postId", verifyToken, deletePost);
 
 export default postRouter;
