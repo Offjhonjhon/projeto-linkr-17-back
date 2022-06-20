0fcaf9bb474b60042fafd4f106b764dfbd92aa57
José Rodolpho <j.rodolpho29@hotmail.com>
2022-06-20 01:49:29 -0300

    fix: change to JSON

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 app.use(router);
 
