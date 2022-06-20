6f66f1d0ea2298248b6dc3a69f36af07b7fcd02e
Offjhonjhon <jjvictorac@gmail.com>
2022-06-20 13:46:45 -0300

    fix: hashtag route

--- controllers/hashtagController.js
+++ controllers/hashtagController.js
@@ -1,4 +1,5 @@
 import connection from "../config/db.js";
+import urlMetadata from 'url-metadata';
 
 export async function getTrendingHashtags(req, res) {
     try {
@@ -19,15 +20,33 @@ export async function getTrendingHashtags(req, res) {
 export async function getHashtagPosts(req, res) {
 
     try {
-        const posts = await connection.query(`
-            SELECT u.avatar, u.name, p.text, p.link as url FROM "publicationsTags" pt
+        const data = await connection.query(`
+            SELECT u.avatar, u.id, u.name, p.text, p.link FROM "publicationsTags" pt
             JOIN publications p ON p."publicationCode" = pt."publicationCode"
             JOIN users u ON p."userId" = u.id
             JOIN tags ON tags.tag = pt.tag
             WHERE tags.tag = $1
         `, [req.params.tag]);
 
-        res.status(200).send(posts.rows);
+        const posts = data.rows;
+
+        const answer = [];
+        for (let i = 0; i < posts.length; i++) { answer.push({}) };
+
+        posts.forEach((post, index) => {
+            urlMetadata(post.link).then(metadata => {
+                answer[index].avatar = post.avatar;
+                answer[index].id = post.id
+                answer[index].name = post.name;
+                answer[index].text = post.text;
+                answer[index].title = metadata.title;
+                answer[index].description = metadata.description;
+                answer[index].url = post.link;
+                answer[index].image = metadata.image;
+                console.log(answer);
+                if (!answer.filter(e => !e.name).length) res.send(answer);
+            })
+        })
     }
     catch {
         res.status(500).send("Error getting posts");
