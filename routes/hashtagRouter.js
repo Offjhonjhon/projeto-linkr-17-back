2fb546d8148cb5d81c6cf8d33846a37cb90fbe08
Offjhonjhon <jjvictorac@gmail.com>
2022-06-20 03:15:10 -0300

    feat: post hashtag route

--- routes/hashtagRouter.js
+++ routes/hashtagRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getTrendingHashtags, getHashtagPosts } from "../controllers/hashtagController.js"
+import { getTrendingHashtags, getHashtagPosts, postPublicationTag } from "../controllers/hashtagController.js"
 
 const hashtagRouter = Router()
 
 hashtagRouter.get("/hashtag/trending-hashtags", getTrendingHashtags)
 hashtagRouter.get("/hashtag/:tag", getHashtagPosts)
+hashtagRouter.post("/hashtag/tag", postPublicationTag)
 
 export default hashtagRouter;
