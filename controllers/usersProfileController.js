20c075b8fb163f884d72631e476dbad60299370a
Offjhonjhon <jjvictorac@gmail.com>
2022-06-24 17:14:43 -0300

    feat: adds the route in which the followers are received

--- controllers/usersProfileController.js
+++ controllers/usersProfileController.js
@@ -1,64 +1,64 @@
 import connection from "../config/db.js"
 import urlMetadata from 'url-metadata';
 
-export async function searchProfile(req,res){
-    const {search, userId} = req.body
-    try{
-        const {rows} = await connection.query('SELECT id, name, avatar FROM users WHERE LOWER(name) LIKE LOWER($1)',[search.name + "%" ])
+export async function searchProfile(req, res) {
+    const { search, userId } = req.body
+    try {
+        const { rows } = await connection.query('SELECT id, name, avatar FROM users WHERE LOWER(name) LIKE LOWER($1)', [search.name + "%"])
 
-        const followed = await connection.query('SELECT "userId", "followUserId" FROM follow WHERE "userId"=$1',[userId])
+        const followed = await connection.query('SELECT "userId", "followUserId" FROM follow WHERE "userId"=$1', [userId])
 
         rows.forEach(row => {
             followed.rows.forEach(follow => {
-                if(row.id === follow.followUserId){
+                if (row.id === follow.followUserId) {
                     row.follow = true
                 }
             })
         })
         console.log(rows)
         res.status(200).send(rows)
-    }catch(e){
+    } catch (e) {
         console.log(e)
         res.status(500).send(e)
     }
 }
 
 export async function getUserProfile(req, res) {
-    const {id} = req.params
+    const { id } = req.params
 
-        try {
+    try {
 
-            const result = await connection.query('SELECT u.avatar, u.id as "userId", u.name, p.text, p.link, p.id as "postId" FROM publications p JOIN users u ON p."userId" = u.id WHERE u.id = $1 ORDER BY p."createdAt" DESC LIMIT 20',[id]);
-            const posts = result.rows
-    
-            if (posts.length === 0) {
-                const {rows} = await connection.query('SELECT users.name FROM users WHERE id=$1',[id])
-                res.send({status:"Empty", name: rows[0].name});
-                return;
-            }
-    
-            const answer = [];
-            for (let i = 0; i < posts.length; i++) { answer.push({}) };
-    
-            posts.forEach((post, index) => {
-                urlMetadata(post.link).then(metadata => {
-                    answer[index].avatar = post.avatar;
-                    answer[index].name = post.name;
-                    answer[index].text = post.text;
-                    answer[index].title = metadata.title;
-                    answer[index].description = metadata.description;
-                    answer[index].url = post.link;
-                    answer[index].image = metadata.image;
-                    answer[index].status = "Filled";
-                    answer[index].id = post.id
-                    answer[index].postId = post.postId
-                    answer[index].userId = post.userId
+        const result = await connection.query('SELECT u.avatar, u.id as "userId", u.name, p.text, p.link, p.id as "postId" FROM publications p JOIN users u ON p."userId" = u.id WHERE u.id = $1 ORDER BY p."createdAt" DESC LIMIT 20', [id]);
+        const posts = result.rows
 
-                    if (!answer.filter(e => !e.name).length) res.send(answer);
-                })
+        if (posts.length === 0) {
+            const { rows } = await connection.query('SELECT users.name FROM users WHERE id=$1', [id])
+            res.send({ status: "Empty", name: rows[0].name });
+            return;
+        }
+
+        const answer = [];
+        for (let i = 0; i < posts.length; i++) { answer.push({}) };
+
+        posts.forEach((post, index) => {
+            urlMetadata(post.link).then(metadata => {
+                answer[index].avatar = post.avatar;
+                answer[index].name = post.name;
+                answer[index].text = post.text;
+                answer[index].title = metadata.title;
+                answer[index].description = metadata.description;
+                answer[index].url = post.link;
+                answer[index].image = metadata.image;
+                answer[index].status = "Filled";
+                answer[index].id = post.id
+                answer[index].postId = post.postId
+                answer[index].userId = post.userId
+
+                if (!answer.filter(e => !e.name).length) res.send(answer);
             })
-    
-    
+        })
+
+
     } catch (error) {
         console.log(`postsGET - ${error}`);
         res.sendStatus(500);
@@ -75,7 +75,7 @@ export async function followUser(req, res) {
 
     try {
         const { rows } = await connection.query('SELECT * FROM follow WHERE "userId" = $1 AND "followUserId" = $2', [userId, userPageId]);
-        
+
         if (rows.length === 0) {
             await connection.query('INSERT INTO follow ("userId", "followUserId") VALUES ($1, $2)', [userId, userPageId]);
             return res.send("followed");
@@ -97,12 +97,25 @@ export async function checkFollow(req, res) {
         const { rows } = await connection.query('SELECT * FROM follow WHERE "userId" = $1 AND "followUserId" = $2', [userId, userPageId]);
 
         if (rows[0]) {
-            return res.send({status: "followed"});
+            return res.send({ status: "followed" });
         } else {
-            return res.send({status: "not followed"});
+            return res.send({ status: "not followed" });
         }
     } catch (e) {
         console.log(e);
         res.status(500).send(e);
+    }
+}
+
+export async function getFollows(req, res) {
+    const { userId } = res.locals;
+
+    try {
+        const { rows } = await connection.query('SELECT * FROM follow WHERE "userId" = $1', [userId]);
+        res.status(200).send(rows);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send(error);
     }
 }
--- routes/usersProfileRouter.js
+++ routes/usersProfileRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
-import { checkFollow, followUser, getUserProfile, searchProfile } from "../controllers/usersProfileController.js"
+import { checkFollow, followUser, getUserProfile, searchProfile, getFollows } from "../controllers/usersProfileController.js"
 import { verifyToken } from "../middlewares/authMiddleware.js"
 
 const usersProfileRouter = Router()
 
 usersProfileRouter.post("/search", searchProfile)
 usersProfileRouter.get("/user/:id", getUserProfile)
-usersProfileRouter.post("/user/follow",  verifyToken, followUser)
+usersProfileRouter.post("/user/follow", verifyToken, followUser)
 usersProfileRouter.post("/check-follow", verifyToken, checkFollow)
+usersProfileRouter.get("/comments/follow", verifyToken, getFollows)
 
 export default usersProfileRouter
