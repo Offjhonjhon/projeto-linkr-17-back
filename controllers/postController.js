7b2ce682a8d7565ee0911bf6be51f5a2adc5f42b
Kethlyn <kethlynsaraa@gmail.com>
2022-06-23 22:04:46 -0300

    feat:  send error messages

--- controllers/postController.js
+++ controllers/postController.js
@@ -30,6 +30,13 @@ export async function postsGET(req, res) {
     const { page, lastUpdateTime } = req.params;
     
     try {
+        const followers = await connection.query('SELECT * FROM follow WHERE "userId" = $1', [userId]);
+        console.log(followers);
+        
+        if (followers.rows.length === 0) {
+            return res.send("You don't follow anyone yet. Search for new friends!")
+        }
+
         const time = lastUpdateTime === "0" ? dayjs().add(1, 'day').format("YYYY-MM-DD HH:mm:ss") : dayjs(lastUpdateTime).add(1, 'second').format("YYYY-MM-DD HH:mm:ss");
         const result = await connection.query(`SELECT u.avatar, u.id ,u.name, p.text, p.link, p.id as "postId", p."createdAt" 
                                                FROM publications p 
@@ -37,7 +44,8 @@ export async function postsGET(req, res) {
                                                JOIN users u ON f."followUserId" = u.id 
                                                WHERE p."createdAt" <= $1 AND f."userId" = $2
                                                ORDER BY p."createdAt" DESC LIMIT 10 OFFSET $3`, [time, userId, page*10]);
-        const posts = result.rows
+        const posts = result.rows;
+        console.log('t', posts)
 
         if (posts.length === 0) {
             res.send("Empty");
